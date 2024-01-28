09a37d1c5eaa642f150d41ed1dfcad00a6d2c9d4
Gate <blade.royalty@gmail.com>
2024-01-29 04:20:37 +0700

    Challange Coding Day 1/366

---
+++ 1-reduce-but-grow.js
@@ -0,0 +1,14 @@
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+
+//   return result;
+// }
+// console.log(grow([2, 2, 2, 2, 2, 2]));
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+
+
+console.log(grow([1, 2, 3, 4, 5]));
